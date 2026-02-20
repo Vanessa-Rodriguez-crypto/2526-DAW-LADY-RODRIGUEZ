00d07880974dacc5e5fc942b699ecfc11b8515c2
Vanessa-Rodriguez-crypto <lv.rodriguezc@uea.edu.ec>
2026-02-20 18:06:07 -0500

    Semana 10 plantillas Jinja2 y rutas dinamicas

--- mi_proyecto_turnos_clinica/app.py
+++ mi_proyecto_turnos_clinica/app.py
@@ -2,18 +2,18 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-# Ruta principal
 @app.route('/')
 def inicio():
     return render_template('index.html')
 
-# Ruta dinámica
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+# RUTA DINÁMICA DEL NEGOCIO (CLÍNICA)
 @app.route('/cita/<paciente>')
 def cita(paciente):
-    return f'Bienvenido {paciente}, tu cita médica ha sido registrada correctamente.'
+    return f'Bienvenido {paciente}. Tu cita médica ha sido registrada correctamente.'
 
-# IMPORTANTE PARA RENDER
 if __name__ == '__main__':
-    import os
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port)
+    app.run(debug=True)
